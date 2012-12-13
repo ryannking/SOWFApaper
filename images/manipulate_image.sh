8103e46b63b5a0a0b9a78a0cea2d7e836b0c5a3a
Spencer Alexander <spencer.r.alexander@gmail.com>
2012-12-12 23:21:47 -0700

    adding images

---
+++ images/manipulate_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for file in `ls *.png`
+do
+   newname=`echo $file | sed 's/\.png$//g'`
+
+   #./png2eps.sh $file > ${newname}.eps
+   #epstopdf ${newname}.eps
+
+   echo %% ${newname} %% >> image_file
+   echo '\begin{figure}' >> image_file
+   echo '\centering' >> image_file
+   echo \\includegraphics[width=\\textwidth]{images/${newname}} >> image_file
+   echo '\caption{INSERT_CAPTION}' >> image_file
+   echo \\label{fig:${newname}} >> image_file
+   echo '\end{figure}' >> image_file
+   echo '' >> image_file
+   echo '' >> image_file
+done
+
+
+
